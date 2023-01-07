2b89368d0c0f23793fed84ead746ed8f5dec379f
WoofPuppyWoof <116989493+WoofPuppyWoof@users.noreply.github.com>
2023-01-06 17:44:02 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var fireworks = [];
 var gravity;
 
 function setup() {
-  createCanvas(400, 300);
+  createCanvas(windowWidth, windowHeight); // 400, 300
   colorMode(HSB);
   gravity = createVector(0, 0.2);
   stroke(255);
